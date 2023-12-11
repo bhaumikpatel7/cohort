714eb488a753c9b7044df284b69d3ddb6e5b8e4b
Bhaumik Patel <72443943+bhaumikpatel7@users.noreply.github.com>
2023-12-11 03:30:25 +0000

    lec 1

--- week1_2.js
+++ week1_2.js
@@ -60,4 +60,27 @@ const user1 = {
 
 }
 
-console.log(user1["gender"]);
+//console.log(user1["gender"]);
+
+function calculateArithmetic(a,b,type){
+    if (type == "sum") {
+
+        const value = sum(a,b)
+        return value
+    }
+    if (type == "minus") {
+        const value = sub(a,b)
+        return value
+
+    }
+}
+
+function sum(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+const value = calculateArithmetic(1,2,sum)
